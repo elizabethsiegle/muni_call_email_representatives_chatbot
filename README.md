9d34e7b52bf9455d7c7bd3d0c72932e1344c0b37
Lizzie Siegle <lizziedev24@gmail.com>
2020-07-15 14:29:11 -0700

    edit code, files, readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## Mass Call and Email your Representatives
+This chatbot was built with Twilio Functions and Twilio Autopilot using JavaScript to mass email all 11 members of the SF County Transportation Authority telling them to defund SFPD and redirect the funds to Muni so the city does not lose 40/68 bus lines!!
+
--- email.js
+++ email.js
@@ -11,19 +11,14 @@ exports.handler = function(context, event, callback) {
     let important_to_you = memory.twilio.collected_data.email_collect.answers.important_to_you.answer.toLowerCase();
     console.log(important_to_you);
     console.log(demands);
-    let new_demands = '';
-    let new_important_to_you = '';
     
     if (demands == 'default') {
-        console.log("in demands default");
-        new_demands = "I demand that billionaires are taxed and funding from the SFPD is redirected to fund public transit and reinstate the 40 Muni bus lines that are meant to be cut as a result of lack of funding. It is routes like the 8, 9, 14, 29, etc.. that serve lower-income communities that are the most PACKED and clearly need MORE lines, not fewer.";
+        demands = "I demand that billionaires are taxed and funding from the SFPD is redirected to fund public transit and reinstate the 40 Muni bus lines that are meant to be cut as a result of lack of funding. It is routes like the 8, 9, 14, 29, etc.. that serve lower-income communities that are the most PACKED and clearly need MORE lines, not fewer.";
     }
     
     if (important_to_you == 'default') {
-        console.log("in default important to you");
-        new_important_to_you = "Every SFUSD student benefits from Muni. A progressive state does not let its most critical services fail. Muni is about accessibility and connecting people--connecting us to family, friends, and what makes SF special.";
+        important_to_you = "Every SFUSD student benefits from Muni. A progressive state does not let its most critical services fail. Muni is about accessibility and connecting people--connecting us to family, friends, and what makes SF special.";
     }
-    var email_body = `Hi {rep name ie. Aaron, Rafael, etc}! My name is ${name}, I live and work in ${live_work}, and my zip code is ${zip_code}. \n\n Keep SF healthy, affordable, liveable, and accessible for all, including working families. The populations who ride Muni the most are the people our city is ALREADY failing to serve. \n Muni is a critical service to SF. Tax billionaires, defund SFPD \n Additionally, 40% of emissions in SF come from transportation. We should not force more cars (and more air pollutants) on the road, especially during a pandemic when people exposed to air pollutants are dying at higher rates.\n ${new_demands} ${new_important_to_you} \n Thank you for your time. ${name}`;
     const messages = [
         {
             to: 'Aaron.Peskin@sfgov.org', 
